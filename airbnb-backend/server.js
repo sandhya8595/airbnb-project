0bf80f81e7111b279d44e8fa8b078e07a13a488e
Sandhya Rajput <isandhya891@gmail.com>
2026-06-30 11:27:11 +0530

    Update CORS origin to production URL

--- airbnb-backend/server.js
+++ airbnb-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://airbnb-project-2-1npu.onrender.com" ,
   credentials: true,
 }));
 app.use(express.json()); // Built-in middleware for parsing JSON
