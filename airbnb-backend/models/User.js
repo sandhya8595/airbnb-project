4a1441e064f065f1aaf73f0ef4e1f12d3987dc8d
Sandhya Rajput <isandhya891@gmail.com>
2026-07-01 06:25:27 +0530

    Fix password hashing by removing unnecessary next() call

--- airbnb-backend/models/User.js
+++ airbnb-backend/models/User.js
@@ -35,7 +35,6 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare entered password with the hashed one in DB
