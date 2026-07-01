30bd6e5356bfca8e8b860d7be6361d606936ae9b
Sandhya Rajput <isandhya891@gmail.com>
2026-07-01 06:57:57 +0530

    Add user reference to reservation schema

--- airbnb-backend/models/Reservation.js
+++ airbnb-backend/models/Reservation.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const reservationSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     homeId: {
       type: String,
       required: true,
